c3d1b675e789e89f64e0c14b9008c63ac3dbd43c
unknown <jtf-charles>
2025-08-13 22:24:06 -0400

    lab completed

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,27 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self,name="Lobo",job="ITC"):
+        self.name=name
+        self.job=job
+
+    def set_name(self,name):
+        if(isinstance(name,str) and len(name) in range(1,26)):
+            self._name=name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    
+    def get_name(self):
+        return self._name
+    
+    def set_job(self,job):
+        if (job in APPROVED_JOBS):
+            self._job=job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    def get_job(self):
+        return self._job
+
+    job=property(get_job,set_job)
+    name=property(get_name,set_name)
